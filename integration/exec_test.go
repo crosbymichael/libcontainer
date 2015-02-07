6f356d567530159a77c773be09df34631d3306e5
Michael Crosby <crosbymichael@gmail.com>
2015-02-06 21:12:27 -0800

    Refactor parent processes into types
    Signed-off-by: Michael Crosby <crosbymichael@gmail.com>

--- integration/exec_test.go
+++ integration/exec_test.go
@@ -15,23 +15,19 @@ func TestExecPS(t *testing.T) {
 	if testing.Short() {
 		return
 	}
-
 	rootfs, err := newRootfs()
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer remove(rootfs)
-
 	config := newTemplateConfig(rootfs)
 	buffers, exitCode, err := runContainer(config, "", "ps")
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("%s: %s", buffers, err)
 	}
-
 	if exitCode != 0 {
 		t.Fatalf("exit code not 0. code %d stderr %q", exitCode, buffers.Stderr)
 	}
-
 	lines := strings.Split(buffers.Stdout.String(), "\n")
 	if len(lines) < 2 {
 		t.Fatalf("more than one process running for output %q", buffers.Stdout.String())
